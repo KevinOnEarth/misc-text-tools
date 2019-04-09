046d8440ee36ac8761267e19c240c95e2fee4c6e
Kevin <kafeman@hotmail.com>
2019-04-08 20:45:36 -0400

    Initial commit

---
+++ spec/misc-text-util-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MiscTextUtilView from '../lib/misc-text-util-view';
+
+describe('MiscTextUtilView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
